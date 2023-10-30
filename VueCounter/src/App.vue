ab664333a6b23e5faa9870b40da46103cc6eaf05
Narsis <narsiskh.2013@gmail.com>
2023-10-30 12:52:45 +0330

    creat the counter with Vue.

---
+++ VueCounter/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref} from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+<main>
+  <div>
+    <h4>
+      The current count is...
+    </h4>
+    <h1>{{ count }}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+  </div>
+</main>
+</template>
+
+<style scoped>
+div {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
